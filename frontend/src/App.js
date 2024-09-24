90c63a59703621a1f60c380de0bd5a678916e08b
Dharmendra Kungsaiya <dharmendra.kungsaiya20@gmail.com>
2024-09-24 15:51:05 +0530

    FE setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,9 @@
 import { Container } from "@mui/material";
 import Navbar from "./Components/AppBar/AppBar";
 import TaskManager  from "./Components/TaskManager/TaskManager";
+export const config = {
+  endpoint: `https://taskmanager-dk.onrender.com/tasks`,
+};
 
 
 const App = () => {
